e1ec0f40ff33e99124edb06efdf535b08b638953
ejthill <ejthill@gmail.com>
2013-10-18 15:51:32 -0500

    SharpTvProxy comments

--- bundles/binding/org.openhab.binding.sharptv/src/main/java/org/openhab/binding/sharptv/internal/hardware/SharpTvProxy.java
+++ bundles/binding/org.openhab.binding.sharptv/src/main/java/org/openhab/binding/sharptv/internal/hardware/SharpTvProxy.java
@@ -33,6 +33,11 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
+/**
+ * Sharp TV Proxy used to control a Sharp TV using TCP
+ * 
+ * @author Eric Thill
+ */
 public class SharpTvProxy {
 
 	private static final int BUTTON_VOL_DOWN = 32;
